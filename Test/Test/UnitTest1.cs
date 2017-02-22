e695a249c84b3fc11b8632d1f6d348eb9884a506
colin-hanson-zocdoc <colin.hanson@zocdoc.com>
2017-02-22 14:25:19 -0500

    Tests

---
+++ Test/Test/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        private Random _rand;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _rand = new Random();    
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            if (_rand.NextDouble() > .5)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            if (_rand.NextDouble() > .5)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            if (_rand.NextDouble() > .5)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestMethod4()
+        {
+            if (_rand.NextDouble() > .5)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
